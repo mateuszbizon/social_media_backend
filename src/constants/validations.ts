eca9533470d4b8b492722ea4b69d67185ea16b88
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-01 11:56:40 +0200

    added sign up dto and validation

---
+++ src/constants/validations.ts
@@ -0,0 +1,10 @@
+export const FIRST_NAME_EMPTY = "First name can't be empty"
+export const LAST_NAME_EMPTY = "Last name can't be empty"
+export const USERNAME_MIN_LENGTH = 8
+export const USERNAME_MAX_LENGTH = 25
+export const USERNAME_MIN_LENGTH_MESSAGE = `Username length must be atleast ${USERNAME_MIN_LENGTH}`
+export const USERNAME_MAX_LENGTH_MESSAGE = `Username length must be maximum ${USERNAME_MAX_LENGTH}`
+export const USERNAME_NO_WHITE_SPACES = "Username can't include white spaces"
+export const PASSWORD_MIN_LENGTH = 8
+export const PASSWORD_MIN_LENGTH_MESSAGE = `Password length must be atleast ${PASSWORD_MIN_LENGTH}`
+export const PASSWORDS_NOT_EQUAL = "Passwords don't match"
