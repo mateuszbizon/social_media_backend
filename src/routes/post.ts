6bac4e1ea0fa88ccee7b6d89bd854c2b35d5cdef
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-02 21:56:58 +0200

    added update post route

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -6,5 +6,6 @@ import { uploadFile } from "../utils/uploadFile"
 const router = express.Router()
 
 router.post("/create-post", authMiddleware, uploadFile.single("image"), createPostController)
+router.patch("/update-post")
 
 export default router
