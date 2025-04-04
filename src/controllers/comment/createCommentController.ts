84b698c1b631b77de2e9c5d607f7bd61896e43a7
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-04 11:33:20 +0200

    added create comment controller

---
+++ src/controllers/comment/createCommentController.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+import { DatabaseError } from "../../errors/DatabaseError";
+import { writeError } from "../../utils/writeError";
+import { commentSchema, CommentSchema } from "../../dtos/commentSchema";
+import { BadRequestError } from "../../errors/BadRequestError";
+import { CreateCommentParams } from "../../types/params";
+import { getPostById } from "../../services/post/getPostById";
+import { NotFoundError } from "../../errors/NotFoundError";
+import { MESSAGES } from "../../constants/messages";
+import { createComment } from "../../services/comment/createComment";
+import { USER_ID } from "../../constants";
+import { CreateCommentResponse } from "../../types/commentResponse";
+
+export async function createCommentController(req: Request<CreateCommentParams, {}, CommentSchema>, res: Response<CreateCommentResponse>, next: NextFunction) {
+    const { content } = req.body
+    const { postId } = req.params
+
+    try {
+        const validationResult = commentSchema.safeParse(req.body)
+
+        if (!validationResult.success) {
+            return next(new BadRequestError(validationResult.error.errors[0].message))
+        }
+
+        const existingPost = await getPostById(postId)
+
+        if (!existingPost) {
+            return next(new NotFoundError(MESSAGES.post.notFound))
+        }
+
+        const createdComment = await createComment({
+            content,
+            postId,
+            authorId: res.locals[USER_ID]
+        })
+
+        res.status(201).json({
+            comment: createdComment
+        })
+    } catch (error) {
+        console.error(error)
+        next(new DatabaseError(writeError(error)))
+    }
+}
--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,7 +1,9 @@
 import express from "express"
+import { authMiddleware } from "../middlewares/authMiddleware"
+import { createCommentController } from "../controllers/comment/createCommentController"
 
 const router = express.Router()
 
-router.post("/create-comment")
+router.post("/create-comment/:postId", authMiddleware, createCommentController)
 
 export default router
