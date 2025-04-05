40a1c3da0507457d0874153b10e75f55ce94f679
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-05 23:47:43 +0200

    added get user profile route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -6,5 +6,6 @@ const router = express.Router()
 
 router.post("/sign-up", signUpController)
 router.post("/sign-in", signInController)
+router.get("/get-user-profile/:username")
 
 export default router
