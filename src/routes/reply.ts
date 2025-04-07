b74c2d1c308a2a57f89c5d8cc9ab5625e49f0dfc
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-07 11:15:51 +0200

    added like reply controller

--- src/routes/reply.ts
+++ src/routes/reply.ts
@@ -2,11 +2,12 @@ import express from "express"
 import { authMiddleware } from "../middlewares/authMiddleware"
 import { createReplyController } from "../controllers/reply/createReplyController"
 import { deleteReplyController } from "../controllers/reply/deleteReplyController"
+import { likeReplyController } from "../controllers/reply/likeReplyController"
 
 const router = express.Router()
 
 router.post("/create-reply/:commentId", authMiddleware, createReplyController)
 router.delete("/delete-reply/:replyId", authMiddleware, deleteReplyController)
-router.patch("/like-reply/:replyId")
+router.patch("/like-reply/:replyId", authMiddleware, likeReplyController)
 
 export default router
--- src/types/replyResponse.ts
+++ src/types/replyResponse.ts
@@ -3,3 +3,7 @@ import { Reply } from "../../generated/prisma"
 export type CreateReplyResponse = {
     reply: Reply
 }
+
+export type LikeReplyResponse = {
+    likedReply: boolean
+}
