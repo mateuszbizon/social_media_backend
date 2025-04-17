1d725f977cd1ebc8e1ea4416d65cac9424d8e88f
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-17 20:50:58 +0200

    added sorting searched users by their followers amount

--- src/services/users/searchUsers.ts
+++ src/services/users/searchUsers.ts
@@ -19,7 +19,7 @@ export async function searchUsers({ page, query }: SearchUsersSearchParams) {
             skip,
             take: limit,
             orderBy: {
-                following: {
+                followers: {
                     _count: "desc"
                 }
             },
