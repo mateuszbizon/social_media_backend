344a8fddddfffbb66ec87a06e16448b47c8fee09
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-14 15:56:17 +0200

    added get chats route

--- src/routes/chats.ts
+++ src/routes/chats.ts
@@ -5,5 +5,6 @@ import { createChatController } from "../controllers/chats/createChatController"
 const router = express.Router()
 
 router.post("/create-chat", authMiddleware, createChatController)
+router.get("/get-chats")
 
 export default router
