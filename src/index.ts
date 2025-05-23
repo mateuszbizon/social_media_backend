dfeebeadf1e6015174c6d0da5a04468b8f465093
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-23 16:14:56 +0200

    Add socket io (#63)
    * added socket io
    
    * added sockets to join chat and send/receive message
    
    * added cors for socket
    
    * added typing sockets
    
    * added sockets to seperate folder

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import bodyParser from "body-parser"
+import { createServer } from "http"
+import { Server } from "socket.io"
 import errorHandler from "./errors/errorHandler"
 import userRoutes from "./routes/users"
 import authRoutes from "./routes/auth"
@@ -9,11 +11,19 @@ import postRoutes from "./routes/post"
 import commentRoutes from "./routes/comment"
 import replyRoutes from "./routes/reply"
 import chatRoutes from "./routes/chats"
+import { chatSocket } from "./sockets/chatSockets"
 
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3001
+const server = createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: process.env.FRONTEND_URL,
+        methods: ["GET", "POST"],
+    }
+})
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -27,7 +37,11 @@ app.use("/chats", chatRoutes)
 
 app.use(errorHandler)
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+    chatSocket(socket)
+})
+
+server.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
 
